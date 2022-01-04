73c03e2a5f7009127362021042063384a131dae5
Aster0123 <danaostapiuk753@gmail.com>
2022-01-04 14:59:37 +0200

    5task

--- web/modules/custom/aster/src/Form/CatsForm.php
+++ web/modules/custom/aster/src/Form/CatsForm.php
@@ -16,7 +16,7 @@ class CatsForm extends FormBase
 
   public function buildForm(array $form, FormStateInterface $form_state)
   {
-    $form['adding_cat'] = [
+    $form['cat_name'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Your cat’s name:'),
       '#placeholder' => $this->t('The name must be in range from 2 to 32 symbols'),
@@ -29,8 +29,20 @@ class CatsForm extends FormBase
     return $form;
   }
 
+
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if ((mb_strlen($form_state->getValue('cat_name')) < 2)) {
+      $form_state->setErrorByName('cat_name', $this->t('Your cat`s name should be more than 2 symbols.'));
+    }
+    if ((mb_strlen($form_state->getValue('cat_name')) > 32)) {
+      $form_state->setErrorByName('cat_name', $this->t('Your cat`s name should be less than 32 symbols.'));
+    }
+  }
+
+
   public function submitForm(array &$form, FormStateInterface $form_state)
   {
     \Drupal::messenger()->addStatus(t('Congratulations! You added your cat!;)'));
   }
+
 }
